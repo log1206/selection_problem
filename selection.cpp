98c2a6074f2c508a1bc468919e7f0112d7d12e0a
106061104 <xup6m4rmp4@gmail.com>
2020-06-05 01:08:45 +0800

    complete

--- selection.cpp
+++ selection.cpp
@@ -3,86 +3,224 @@
 
 using namespace std;
 
+
+#define superbig 30001
+
 // input size n 5-15
 //cost range 1-99
 
+int n; //n input range from 10000 - 30000
+int k; //kth big
+vector<int> arr; //storing all number
+
+// below are quicksort part for vector-----------------------
+void swap(vector<int>& vec, int head, int tail) {      //swap function
+    int temp = vec[head];
+    vec[head] = vec[tail];
+    vec[tail] = temp;
+
+}
+void Qsort(vector<int> &vec, int head, int tail) {
+    int i, j, mid, piv;
+    i = head;
+    j = tail;
+    mid = head + (tail - head) / 2;
+    piv = vec[mid];
+
+    while (i<tail || j>head) {
+        while (vec[i] < piv)
+            i++;
+        while (vec[j] > piv)
+            j--;
+
+        if (i <= j) {
+            swap(vec, i, j); //error=swap function doesnt take 3 arguments
+            i++;
+            j--;
+        }
+        else {
+            if (i < tail)
+                Qsort(vec, i, tail);
+            if (j > head)
+                Qsort(vec, head, j);
+            return;
+        }
+    }
+}
+
+//---------------------------------------- 
+
+int select(vector<int> arrp, int number, int rank){
 
 
-void TSP_brute(){   
+   
+    int need = number%5;
+  
+    if(need >0)
+        need = 5 - need;
+    number += need;
+    for(int i =0;i<need;i++){
+        arrp.push_back(superbig);
+    }    
+
+    if(number <=5){
+        Qsort(arrp,0,arrp.size()-1);  // directly brute force search   
+
+        return arrp[rank-1];
+    }
+    else{
+       
+        vector<int> middle;       //sort each 5 item group and sort all group by median 
+        vector<int> tmp;
+        for(int i=0;i<number/5;i++){
+         
+            for(int j=0;j<5;j++){
+              tmp.push_back(arrp[i*5+j]);
+            }
+            Qsort(tmp,0,tmp.size()-1);
+
+            middle.push_back(tmp[2]);
+
+            for(int j=0;j<5;j++)
+                tmp.pop_back();
+            
+        }
+        Qsort(middle,0,middle.size()-1);
+
+
+    
+        int median = middle[number/10-1];
+        int small =0;
+        int equal =0;
+        int big   =0;
+        vector<int> arrb;
+        vector<int> arrs;
+        for(int i=0;i<number;i++){
+            if(arrp[i] > median && arrp[i] != 30001){
+                arrb.push_back(arrp[i]);
+                big++;
+            }
+            else if(arrp[i] < median){
+                arrs.push_back(arrp[i]);
+                small++;
+            }
+           
+           equal = number -small -big -need;
+        }
+
+        // next recursive
+
+        if(rank < small)  return select(arrs, small, rank);
+        else if(rank < small +equal) return median;
+        else return select(arrb, number-small-equal-need, rank-small-equal);
+        
+    }
+
+
+}
+
+void select_ps(){   
 
     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 
-    
-    vector<int> path; //path for this round 
-    for(int i=1; i <n ;i++){    //initialization: load every node to path except first node 0
-            path.push_back(i);
-    }
-    int cost =INT_MAX; //initial biggest number to minimum cost
-    vector<int> final_path;
-    do{
-                
-        int node =0; //head = 0 in array
-        int cur_cost = 0; // current path cost
-        for(int i=0;i < path.size();i++){
-            cur_cost += graph[node][path[i]];
-            node = path[i];
-        }
-        cur_cost += graph[node][0]; // add the cost of last node to head
-        if(cost > cur_cost){
-            final_path = path;  //maybe has problem
-            cost =cur_cost;
-        }
-    }
-    while(next_permutation(path.begin(),path.end()));
+    //pre-add some superbig to make array can be divided by 5
+   
+    int answer = select(arr,n,k);
 
     // calculate time
     std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
     double diff = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
     diff /= 1000000;
-
-    // save to file
-    fstream file;
-    file.open("BF.txt",ios::out);
-    file.setf(ios::fixed);
     
-    file << "Solution : 0 - ";
-    for(int i=0;i<final_path.size();i++){
-        file << final_path[i] <<" - ";
-    }
-    file << "0" << endl;
-    file << "Cost     : "<< cost << endl;
-    file << "Time     : " << setprecision(6)<<diff<< endl;
+    // save to file
+    
+    fstream file;
+    file.open("PS.txt",ios::out);
+    file.setf(ios::fixed);
+    file << answer << endl;
+    file << setprecision(6)<<diff<< endl;
     file.close();
 }
 
 
-    
+/*
+
+void exchange(int *l, int *r){    //change
+    int temp = *l;
+    *l = *r;
+    *r = temp;
+}
+int Partition(int *arr_fqs, int head, int tail){
+    int pivot = arr_fqs[tail];
+    int j = head -1;
+    for (int i = head; i < tail; i++) {
+        if (arr_fqs[i] < pivot) {
+            j++;
+            exchange(&arr_fqs[j],&arr_fqs[i]);  //any smaller than pivot should put to left
+        }
+    }
+    j++;
+    exchange(&arr_fqs[j], &arr_fqs[tail]); // put pivot to right position
+    return j;
+}
+void Qsort(int *arr_fqs,int head, int tail){
+    if (head < tail) {
+        int pivot = Partition(arr_fqs,head, tail);
+        Qsort(arr_fqs, head, pivot - 1);
+        Qsort(arr_fqs, pivot + 1, tail);
+    }
+}
+ 
+
+*/
+
 
 void readFile(){
  
     fstream file; 
+    int tmp;
     file.open("input.txt",ios::in);
-    file >> n;  //n for city number
-    graph = new int*[n];
-    for(int i = 0; i < n; ++i)  //initial each line
-        graph[i] = new int[n];
-
-    for(int i=0; i<n;i++)      //input each line
-        for(int j=0;j<n;j++)
-            file >> graph[i][j];
+    file >> n;  //n for total number
+    file >> k; //k for kth big
+   
+    for(int i=0;i<n;i++){
+         file >> tmp;
+         arr.push_back(tmp);
+        
+    }
     file.close();
 }
 
+
+void select_quick(){
+
+     std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    vector<int> arrq =arr;
+    Qsort(arrq, 0,n-1);
+    // calculate time
+    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+    double diff = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+    diff /= 1000000;
+    
+    // save to file
+    fstream file;
+    file.open("QS.txt",ios::out);
+    file.setf(ios::fixed);  
+    file <<arrq[k-1] << endl; //number
+    file << setprecision(6)<<diff<< endl; //time
+    file.close();
+
+}
 
 
 int main(void){
 
     readFile();
-
-    
+  
     select_quick();
+    select_ps();
+
  
 
     return 0;
 }
-
---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> // all library
+#include <chrono>
+
+using namespace std;
+
+int n;
+int asd(int q){
+    cout << q;
+}
+
+int main(void){
+
+    n =3;
+  asd(3);
+ 
+
+    return 0;
+}
